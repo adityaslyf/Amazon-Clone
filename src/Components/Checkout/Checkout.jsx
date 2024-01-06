916611eaa72438c417374dad552f9d639f5e1a1a
adityaslyf <aditya.varshneymail@gmail.com>
2024-01-06 23:26:47 +0530

    chnage in reducer

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -1,15 +1,32 @@
 import Subtotal from "./Subtotal/Subtotal";
 import "./checkout.css";
 import CheckoutProduct from "./CheckoutProduct/CheckoutProduct";
+import { useStateValue } from "../../StateProvider";
+
 const Checkout = () => {
+  const [{  basket}, dispatch] =useStateValue();
+
+
   return (
     <div className="checkout">
       <div className="checkout__left">
         <img src="" alt="" />
         <div>
           <h2 className="checkout__title">Your Shopping Basket</h2>
+          {basket.map(item=>(
+            <CheckoutProduct
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            image={item.image}
+            price={item.price}
+            rating={item.rating}
+            />
+          ))}
 
         </div>
+       
+      
       </div>
       <div className="checkout__right">
         <h2>The subtotal will appear here</h2>
--- src/Components/Checkout/CheckoutProduct/CheckoutProduct.jsx
+++ src/Components/Checkout/CheckoutProduct/CheckoutProduct.jsx
@@ -1,13 +1,35 @@
 import "./CheckoutProduct";
-import '../../Home/Home'
+import "../../Home/Home";
 import Subtotal from "../Subtotal/Subtotal";
 import { useStateValue } from "../../../StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
+  const [{  basket}, dispatch] =useStateValue();
+  const removeBasket=()=>{
+//remove the basket
+dispatch({
+  type:'REMOVE_FROM_BASKET',
+  id: id,
+})
+  }
   return (
     <div>
-      <img src={image} alt="" />
-      
+      <img src={image} alt=""  className=" w-44"/>
+      <p className=" w-66">{title}</p>
+      <p>
+        <small>$</small>
+        <strong>{price}</strong>
+      </p>
+      <div className=" flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <p>⭐</p>
+          ))}
+      </div>
+      <div onClick={removeBasket} className=" border-orange-500 bg-orange-500  w-32 border-r-4">
+          Remove the item
+        </div>
     </div>
   );
 }
